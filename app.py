99494599d9c9d3f27469aa21c335dd4c04ddc628
haveneshb15 <31435036+haveneshb15@users.noreply.github.com>
2025-02-14 07:05:15 +0000

    commit

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, request, render_template # type: ignore
+import google.generativeai as genai
+import os
+
+api = os.getenv("makerspace")
+genai.configure(api_key=api)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 app = Flask(__name__)
 
@@ -10,6 +16,13 @@ def index():
 @app.route("/makersuite", methods=['GET','POST'])
 def makersuite():
     return render_template("makersuite.html")
+
+@app.route("/gemini", methods=['GET','POST'])
+def gemini():
+    q = request.form.get("q")
+    while not q == "quit":
+        r = model.generate_content(q)
+        return render_template("gemini.html", r=r.candidates[0].content.parts[0].text)
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0")
