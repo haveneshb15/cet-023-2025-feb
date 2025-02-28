1dddc0b1ae631d49044d6ae5e0933383f1624e57
haveneshb15 <31435036+haveneshb15@users.noreply.github.com>
2025-02-28 02:56:54 +0000

    add textblob

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def sentiment():
 @app.route("/textblob_result", methods=['GET','POST'])
 def textblob_result():
     q = request.form.get("q")
-    r = textblob.Textblob(q).sentiment
+    r = textblob.TextBlob(q).sentiment
     return render_template("textblob_result.html", r=r.candidates[0].content.parts[0].text)
 
 if __name__ == "__main__":
